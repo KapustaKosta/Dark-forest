201b47aa2d76d5d9310e785eecd5daf242974a12
Michael2464 <mzyranov26@gmail.com>
2022-11-28 18:31:02 -0800

    Контроль над движением врага (AStarPathfinding)

--- Assets/Scripts/EnemyGraphics.cs
+++ Assets/Scripts/EnemyGraphics.cs
@@ -5,20 +5,30 @@ using Pathfinding; // A* algorithm
 
 public class EnemyGraphics : MonoBehaviour
 {
-    public AIPath m_AIPath;
+    private AIPath m_AIPath;
+    public Transform m_PlayerTransform;
+
+    private void Start()
+    {
+        m_AIPath = GetComponent<AIPath>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // Пример остановки врага
+        if(Input.GetKeyDown(KeyCode.J))
+            m_AIPath.canMove = !m_AIPath.canMove;
+
         // Это будет поворачивать врага влево или вправо,
         // в зависимости от направления движения
-        if(m_AIPath.desiredVelocity.x >= 0.01f)
+        if (m_AIPath.desiredVelocity.x >= 0.01f)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            m_PlayerTransform.localScale = new Vector3(-1f, 1f, 1f);
         }
         else if(m_AIPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            m_PlayerTransform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
 }
